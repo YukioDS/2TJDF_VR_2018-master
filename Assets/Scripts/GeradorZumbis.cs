e833e7306ac87a4cfea1287580f5f812f03569ed
YukioDS <yukiohayashi19@gmail.com>
2018-04-03 11:58:24 -0300

    Animacao Zumbi

--- Assets/Scripts/GeradorZumbis.cs
+++ Assets/Scripts/GeradorZumbis.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class GeradorZumbis : MonoBehaviour {
 
+    public float intervalo = 3f;
+    public float area = 4f;
+    public GameObject zumbiOrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+        InvokeRepeating("GerarZumbi", intervalo, intervalo);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void GerarZumbi()
+    {
+        GameObject zumbi = Instantiate(zumbiOrefab);
+        Vector2 posicaoAleatoria = Random.insideUnitCircle * area;
+        zumbi.transform.position = new Vector3(posicaoAleatoria.x, 0f, posicaoAleatoria.y);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
--- Assets/Scripts/Zumbi.cs
+++ Assets/Scripts/Zumbi.cs
@@ -6,10 +6,16 @@ public class Zumbi : MonoBehaviour {
 
     private bool andando = false;
     public float delayAndar = 1.5f;
-
+    public Rigidbody rb;
+    private float velocidade = 0.35f;
+    private GameObject jogador;
     // Use this for initialization
     void Start ()
     {
+        jogador = GameObject.FindGameObjectWithTag("Player");
+
+        transform.LookAt(jogador.transform);
+
         Invoke("Andar", delayAndar);
 	}
 
@@ -21,6 +27,9 @@ public class Zumbi : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	    	
+        if (andando)
+        {
+            rb.velocity = (jogador.transform.position - transform.position).normalized * velocidade;
+        }
 	}
 }
